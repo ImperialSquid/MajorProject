14f11db673dcc6fe0943c1e8f99f656885ef3346
ImperialSquid <benjaeger99@gmail.com>
2020-03-05 10:17:54 +0000

    tweaked weights on most similar search, added log line for team sizes
    Signed-off-by: ImperialSquid <benjaeger99@gmail.com>

--- spymaster.py
+++ spymaster.py
@@ -10,14 +10,18 @@ log.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', datefmt="%d/
 log.info("##### - Program started - #####")
 
 log.info("Loading word models...")
-log.debug("Loading Google News")
-wordModel = KeyedVectors.load_word2vec_format(r"C:\Users\benja\gensim-data\word2vec-google-news-300\word2vec-google-news-300.gz", binary=True, limit=500000)
+# log.debug("Loading Google News 300")
+# wordModel = KeyedVectors.load_word2vec_format(r"C:\Users\benja\gensim-data\word2vec-google-news-300\word2vec-google-news-300.gz", binary=True, limit=500000)
+# log.debug("Loading Twitter 100")
+# wordModel = KeyedVectors.load_word2vec_format(r"C:\Users\benja\gensim-data\glove-twitter-100\glove-twitter-100.gz", limit=500000)
+log.debug("Loading Wiki 100")
+wordModel = KeyedVectors.load_word2vec_format(r"C:\Users\benja\gensim-data\glove-wiki-gigaword-100\glove-wiki-gigaword-100.gz", limit=500000)
 log.info("Done loading models")
 
 log.info("Loading words and settings")
 log.debug("Loading words")
 targetWords = [w.strip() for w in open("targets.txt", "r").readlines()]
-targetWords = [w for w in targetWords if w in wordModel.vocab]
+targetWords = [w for w in targetWords if w in wordModel.vocab]  # strips out words from the targetWords lists that don't appear in the model vocab
 print(len(targetWords), targetWords)
 log.debug("Done loading words")
 
@@ -26,14 +30,15 @@ teamSizes = [line.strip() for line in open("teams.txt").readlines()]
 teamSizes = {t.split(":")[0]: t.split(":")[1] for t in teamSizes}
 teamSizes = {k: int(teamSizes[k]) for k in teamSizes.keys()}
 teamSizes["grey"] = max(0, 25-sum(teamSizes.values()))
-pp.pprint(teamSizes)
+log.debug("Team sizes: {0}".format(" - ".join([k+":"+str(teamSizes[k]) for k in sorted(teamSizes.keys())])))
 log.debug("Done loading settings")
 log.info("Done loading words and settings")
 
-gameRuns = 1
+gameRuns = 3
 log.info("Running {0} games".format(gameRuns))
 for i in range(gameRuns):
     log.debug("Game {0}:".format(i))
+
     shuffle(targetWords)
     nameGen = iter(targetWords)
     teamNames = dict()
@@ -43,14 +48,14 @@ for i in range(gameRuns):
 
     hints = dict()
     for red in teamNames["red"]:
-        hints[red] = wordModel.most_similar(positive=[[red, 10]],
-                                            negative=[[word, -1.0] for word in teamNames["grey"]] +
-                                                     [[word, -3.0] for word in teamNames["blue"]] +
-                                                     [[word, -7.0] for word in teamNames["black"]],
+        hints[red] = wordModel.most_similar(positive=[(red, 30)],
+                                            negative=[(word, -1) for word in teamNames["grey"]] +
+                                                     [(word, -3) for word in teamNames["blue"]] +
+                                                     [(word, -7) for word in teamNames["black"]],
                                             topn=5)
     log.debug("Hints:")
     for red in teamNames["red"]:
-        log.debug("{0}: {1}".format(red, "//".join([" - ".join([h[0], "{0:3.3f}".format(h[1])]) for h in hints[red]])))
+        log.debug("{0}: \t{1}".format(red, "\t//\t".join([" - ".join([h[0], "{0:3.3f}".format(h[1])]) for h in hints[red]])))
     print("Hints: ")
     pp.pprint(hints)
 
