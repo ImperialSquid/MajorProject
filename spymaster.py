83e6c8adb742511a02a99407253d937d6f255a64
ImperialSquid <benjaeger99@gmail.com>
2020-03-05 11:40:39 +0000

    continued process of making SpyMaster class, renamed targets.txt to game_words.txt as it was more appropriate
    Signed-off-by: ImperialSquid <benjaeger99@gmail.com>

--- spymaster.py
+++ spymaster.py
@@ -4,10 +4,11 @@ from gensim.models import KeyedVectors
 
 
 class SpyMaster:
-    def __init__(self, teams_file="teams.txt", targets_file="targets.txt"):
+    def __init__(self, teams_file="teams.txt", words_file="game_words.txt"):
         log.info("SpyMaster Initialised")
         self.teams = self.load_teams(teams_file)
-        self.word_model = self.load_word_model()
+        self.word_model = self.load_word_model(model_name="")
+        self.game_words = self.load_game_words(words_file)
 
     def load_teams(self, teams_file="teams.txt"):
         log.info("Getting teams sizes...")
@@ -21,11 +22,11 @@ class SpyMaster:
                 teams[k] = int(splits[k])
                 log.debug("Found value for {0}: {1}".format(k, teams[k]))
             except KeyError as e:
-                log.debug("No value found for {}, using default".format(k))
+                log.warning("No value found for {}, using default".format(k))
         teams["grey"] = max(0, 25 - sum(teams.values()))
 
-        log.info("Team sizes: {0}".format(" - ".join([k + ":" + str(teams[k]) for k in sorted(teams.keys())])))
-
+        log.info("Done loading team sizes")
+        log.debug("Team sizes: {0}".format(" - ".join([k + ":" + str(teams[k]) for k in sorted(teams.keys())])))
         return teams
 
     def load_word_model(self, model_name):
@@ -46,18 +47,21 @@ class SpyMaster:
                 r"C:\Users\benja\gensim-data\glove-wiki-gigaword-100\glove-wiki-gigaword-100.gz", limit=500000)
 
         log.info("Done loading models")
-
         return word_model
 
+    def load_game_words(self, words_file="game_words.txt"):
+        log.info("Loading game words...")
+        file_words = [w.replace(" ","_").strip() for w in open(words_file, "r").readlines()]
+        game_words = [w for w in file_words if w in self.word_model.vocab]
+        missing = [w for w in file_words if w not in game_words]
+        log.info("Done loading game words")
+        log.debug("Loaded {0} words, {1} missing (missing word(s): {2})".format(len(game_words), len(missing),
+                                                                                ", ".join(missing)))
+        return game_words
+
 r"""
-log.info("##### - Program started - #####")
-
-
-
 log.info("Loading words and settings")
 log.debug("Loading words")
-targetWords = [w.strip() for w in open("targets.txt", "r").readlines()]
-targetWords = [w for w in targetWords if w in wordModel.vocab]  # strips out words from the targetWords lists that don't appear in the model vocab
 print(len(targetWords), targetWords)
 log.debug("Done loading words")
 
