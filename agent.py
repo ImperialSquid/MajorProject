c6734db233dc66d01f8633b95d97a67c14b54e41
ImperialSquid <benjaeger99@gmail.com>
2020-03-25 02:09:57 +0000

    added three wordnet based eval methods (note: these don't work for verb-noun comparisons)
    Signed-off-by: ImperialSquid <benjaeger99@gmail.com>

--- agent.py
+++ agent.py
@@ -3,6 +3,8 @@ from itertools import chain
 from time import sleep
 
 import requests as req
+from nltk.corpus import wordnet as wn
+from nltk.corpus.reader import WordNetError
 
 
 class Agent:
@@ -11,10 +13,11 @@ class Agent:
         self.team_words = dict()
         self.board_words = list()
         self.hints = dict()
-        self.settings = self.__load_settings(sett_file="settings.txt",
+        self.settings = self.__load_settings(sett_file="settings/settings.txt",
                                              default_dict={"max_top_hints": 10,
                                                            "max_levels": 2})
-        self.evaluation_methods = [self.__word_net_eval]
+        self.evaluation_methods = [self.__concept_net_eval, self.__word_net_path_eval,
+                                   self.__word_net_wup_eval, self.__word_net_lch_eval]
         log.info("Agent initialised!")
 
     def __load_settings(self, sett_file: str, default_dict: dict):
@@ -72,10 +75,12 @@ class Agent:
             for team in sorted(self.team_words.keys()):
                 outf.write("{0}: {1}\n".format(team, " - ".join(self.team_words[team])))
             outf.write("Hint Evaluation:\n")
+            outf.write("Note: Due to WordNet's Structure, noun to verb comparison is impossible,\n" +
+                       "you cannot compare apple to eat, only apple to orange or eat to devour etc\n")
             for level in self.hints.keys():
                 log.info("Level {0} hints".format(level))
                 outf.write("Level: {0}\n".format(level))
-                for num, hint in enumerate(self.hints[level][:1]):
+                for num, hint in enumerate(self.hints[level]):
                     log.info("Evaluating hint {0} of {1}: {2}".format(num + 1, len(self.hints[level]), hint[1][0]))
                     outf.write("Target: {0:30} Hint: {1:20} WM Score: {2:20f}\n".format(",".join(hint[0]),
                                                                                         hint[1][0], hint[1][1]))
@@ -88,20 +93,62 @@ class Agent:
                                                     key=lambda x: x[1],
                                                     reverse=True)
                         score_str = " - ".join(
-                            ["{0}-{1}".format(word[0], word[1]) for word in sorted_board_words])
+                            ["{0}-{1:.3f}".format(word[0], word[1]) for word in sorted_board_words])
                         outf.write("Ranked by {0}: {1}\n".format(method.__name__, score_str))
 
-    def __word_net_eval(self, hint: str, target: str):
+    def __concept_net_eval(self, hint: str, target: str):
         relatedness_url = "http://api.conceptnet.io/relatedness?node1=/c/en/{}&node2=/c/en/{}"
         json = req.get(relatedness_url.format(hint, target)).json()
         sleep(2)
         return json["value"]
 
+    def __word_net_path_eval(self, hint: str, target: str):
+        h_synsets = wn.synsets(hint)
+        t_synsets = wn.synsets(target)
+        lst = []
+        for h in h_synsets:
+            for t in t_synsets:
+                strength = wn.path_similarity(h, t)
+                lst.append(strength if strength is not None else -1)
+        if all([x == -1 for x in lst]):
+            return -9.999
+        else:
+            return max(lst)
+
+    def __word_net_lch_eval(self, hint: str, target: str):
+        h_synsets = wn.synsets(hint)
+        t_synsets = wn.synsets(target)
+        lst = []
+        for h in h_synsets:
+            for t in t_synsets:
+                try:
+                    strength = wn.lch_similarity(h, t)
+                except WordNetError:
+                    strength = -1
+                lst.append(strength if strength is not None else -1)
+        if all([x == -1 for x in lst]):
+            return -9.999
+        else:
+            return max(lst)
+
+    def __word_net_wup_eval(self, hint: str, target: str):
+        h_synsets = wn.synsets(hint)
+        t_synsets = wn.synsets(target)
+        lst = []
+        for h in h_synsets:
+            for t in t_synsets:
+                strength = wn.wup_similarity(h, t)
+                lst.append(strength if strength is not None else -1)
+        if all([x == -1 for x in lst]):
+            return -9.999
+        else:
+            return max(lst)
+
 
 if __name__ == "__main__":
     root_logger = log.getLogger()
     root_logger.setLevel(log.DEBUG)
-    handler = log.FileHandler("agent-log.txt", "a", "utf-8")
+    handler = log.FileHandler("logs/agent-log.txt", "a", "utf-8")
     handler.setFormatter(log.Formatter("%(asctime)s : %(levelname)s : %(message)s", datefmt="%d/%m - %H:%M:%S"))
     root_logger.addHandler(handler)
 
